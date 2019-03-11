b999ae32bd9c22cc1c49778d764d2250d06a85d6
dustinmcgilvray <dustinmcgilvray@gmail.com>
2019-03-11 12:25:00 -0500

    Added random images folder and random image generators. Enhanced codefor proper play through.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,123 +1,166 @@
 //Global Variables
 var goal = randomNum(19, 120);
-$("#goalvalue").text(goal);
+$("#goal-value").text(goal);
 var win = 0;
-$("#winvalue").text(win);
+$("#win-value").text(win);
 var lose = 0;
-$("#losevalue").text(lose);
+$("#lose-value").text(lose);
 var score = 0;
-$("#scorevalue").text(score);
+$("#score-value").text(score);
 var image1 = randomNum(1, 12);
 var image2 = randomNum(1, 12);
 var image3 = randomNum(1, 12);
 var image4 = randomNum(1, 12);
 
-//Random Number Funtion 
+//Random Number Function 
 function randomNum(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 //Start Function
 function gameStart() {
-goal = randomNum(19, 120);
-$("#goalvalue").text(goal);
-score = 0;
-$("#scorevalue").text(score);
-image1 = randomNum(1, 12);
-image2 = randomNum(1, 12);
-image3 = randomNum(1, 12);
-image4 = randomNum(1, 12);
+  score = 0;
+  $("#score-value").text(score);
+  goal = randomNum(19, 120);
+  $("#goal-value").text(goal);
+  image1 = randomNum(1, 12);
+  image2 = randomNum(1, 12);
+  image3 = randomNum(1, 12);
+  image4 = randomNum(1, 12);
+  randomImage1();
+  randomImage2();
+  randomImage3();
+  randomImage4();
 }
 
 //Win Counter Function
-function wincounter() {
+function winCounter() {
   win++;
-  $("#winvalue").text(win);
+  $("#win-value").text(win);
   alert("Congratulations! You chose wisely.")
   gameStart();
+  gameRestart();
 }
 
 //Lose Counter Function
-function losecounter() {
+function loseCounter() {
   lose++;
-  $("#losevalue").text(lose);
+  $("#lose-value").text(lose);
   alert("You Chose Poorly.")
   gameStart();
+  gameRestart();
 }
 
-// Re-start Function
+// Re-start Function (Reset)
 function gameRestart() {
-  if (win<4) {
-    win=0;
-    $("#winvalue").text(win);
-    lose=0;
-    $("#losevalue").text(lose);
-
+  if (win > 4) {
+    win = 0;
+    $("#win-value").text(win);
+    lose = 0;
+    $("#lose-value").text(lose);
     gameStart();
-  }
-  else if (lose<4) {
-    win=0;
-    $("#winvalue").text(win);
-    lose=0;
-    $("#losevalue").text(lose);
+    randomImage1();
+    randomImage2();
+    randomImage3();
+    randomImage4();
+  } else if (lose > 4) {
+    win = 0;
+    $("#win-value").text(win);
+    lose = 0;
+    $("#lose-value").text(lose);
     gameStart();
+    randomImage1();
+    randomImage2();
+    randomImage3();
+    randomImage4();
   }
 }
 
 //On-Click Event for Images
 $("#image-card-1").on("click", function () {
   score += image1;
-  $("#scorevalue").text(score);
+  $("#score-value").text(score);
   if (score === goal) {
-    wincounter();
-  }
-  else if (score > goal) {
-    losecounter();
+    winCounter();
+  } else if (score >= goal) {
+    loseCounter();
   }
 });
 
-$("#image-card-2").on("click", function() {
+$("#image-card-2").on("click", function () {
   score += image2;
-  $("#scorevalue").text(score);
+  $("#score-value").text(score);
   if (score === goal) {
-    wincounter();
-  }
-  else if (score > goal) {
-    losecounter();
+    winCounter();
+  } else if (score > goal) {
+    loseCounter();
   }
 });
 
-$("#image-card-3").on("click", function() {
+$("#image-card-3").on("click", function () {
   score += image3;
   $("#scorevalue").text(score);
   if (score === goal) {
-    wincounter();
-  }
-  else if (score > goal) {
-    losecounter();
+    winCounter();
+  } else if (score > goal) {
+    loseCounter();
   }
 });
 
-$("#image-card-4").on("click", function() {
+$("#image-card-4").on("click", function () {
   score += image4;
-  $("#scorevalue").text(score);
+  $("#score-value").text(score);
   if (score === goal) {
-    wincounter();
-  }
-  else if (score > goal) {
-    losecounter();
+    winCounter();
+  } else if (score > goal) {
+    loseCounter();
   }
 });
 
 //Instruction Slide In and Out
-$(document).ready(function(){
-  $(".fa-times").click(function(){
-      $(".sidebar_menu").addClass("hide_menu");
-      $(".toggle_menu").addClass("opacity_one");
+$(document).ready(function () {
+  $(".fa-times").click(function () {
+    $(".sidebar_menu").addClass("hide_menu");
+    $(".toggle_menu").addClass("opacity_one");
   });
 
-  $(".toggle_menu").click(function(){
-      $(".sidebar_menu").removeClass("hide_menu");
-      $(".toggle_menu").removeClass("opacity_one");
+  $(".toggle_menu").click(function () {
+    $(".sidebar_menu").removeClass("hide_menu");
+    $(".toggle_menu").removeClass("opacity_one");
   });
 });
+
+
+//Random Image Generators for Cards
+function randomImage1() {
+  var randomImage = randomNum(1, 120)
+  
+  var image = document.getElementById("myImage1");
+  var path = "./assets/images/random_cards/gpk_" + randomImage + ".jpg";
+  image.src = path;
+}
+
+function randomImage2() {
+  var randomImage = randomNum(1, 120)
+  
+  var image = document.getElementById("myImage2");
+  var path = "./assets/images/random_cards/gpk_" + randomImage + ".jpg";
+  
+  image.src = path;
+}
+
+function randomImage3() {
+  var randomImage = randomNum(1, 120)
+  
+  var image = document.getElementById("myImage3");
+  var path = "./assets/images/random_cards/gpk_" + randomImage + ".jpg";
+  image.src = path;
+}
+
+function randomImage4() {
+  var randomImage = randomNum(1, 120)
+  
+  var image = document.getElementById("myImage4");
+  var path = "./assets/images/random_cards/gpk_" + randomImage + ".jpg";
+  image.src = path;
+}
